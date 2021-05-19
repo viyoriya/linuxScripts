b069d86f8d70abffe04cc6606d249e0dff68503d
viyoriya <viyoriya@gmail.com>
2021-05-19 21:51:00 +0100

    updated 19-05-2021

--- gtkExctract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gresource list gtk.gresource > extracted.txt
-strlines=`cat extracted.txt`
-for line in $strlines ; do
-	sudo gresource extract gtk.gresource $line > $(basename $line)
-done
-
-#/org/adapta-project/gtk-3.24-nokto-eta/assets/
