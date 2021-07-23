461e616af82b319f68b38f72e0bc98ef566ffdbe
viyoriya <viyoriya@gmail.com>
2021-07-23 09:27:18 +0100

    Updated 23-07-2021

--- gitPullCli.sh
+++ gitPullCli.sh
@@ -2,10 +2,11 @@
 
 # Init : github.com/viyoriya
 
-# find all the git repos and store it in the txt file (excluding .tmux or .cache or .config)
+# find all the git repos excluding .tmux or .cache or .config
+#find ~/ \( -name .tmux -o -name .cache -o -name .config \) -prune -false -o -name .git -printf '%h\n' > gitRepoList.txt
 
-
-find ~/ \( -name .tmux -o -name .cache -o -name .config \) -prune -false -o -name .git -printf '%h\n' > gitRepoList.txt
+# find all the git repos  
+find ~/ -name .git -printf '%h\n' > gitRepoList.txt
 
 R_LIST=gitRepoList.txt
 
