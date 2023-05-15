003079251c0f003e3368769779b65fca2f529cd5
viyoriya <viyoriya@gmail.com>
2023-05-15 10:15:50 +0100

    Updated bash n tube script

--- tubescript.py
+++ tubescript.py
@@ -1,6 +1,6 @@
 import requests
 
-tflResponse = requests.get('https://api.tfl.gov.uk/Line/Mode/tube%2Cdlr%2Coverground/Status?detail=true')
+tflResponse = requests.get('https://api.tfl.gov.uk/Line/Mode/tube%2Cdlr%2Coverground%2Celizabeth-line/Status?detail=true')
 if tflResponse.status_code != 200:
 	print('{} {}'.format('Error Status code ==>-',tflResponse.status_code))    
 for tubeLines in tflResponse.json():
